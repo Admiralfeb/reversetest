70fc9ccd828b74a5a00a64b723d0cfb07c14765f
Zachary Austin <Zachary@admiralfeb.net>
2022-01-28 19:02:04 -0600

    update reversephrase to do fully in place

--- reversetest/Class1.cs
+++ reversetest/Class1.cs
@@ -16,6 +16,41 @@ public static class ReverseTest
 
     public static void ReversePhrase(ref char[] phrase)
     {
+        // flip the whole array
+        ReverseWord(ref phrase);
+
+        // Find and reverse each word within the array
+        for (int i = 0; i < phrase.Length; i++)
+        {
+            if (phrase[i] == ' ')
+                continue;
+
+            for (int j = i; j < phrase.Length; j++)
+            {
+                if (phrase[j] == ' ' || j == phrase.Length - 1)
+                {
+                    if (j == phrase.Length - 1)
+                    {
+                        j++;
+                    }
+                    // perform swap
+                    for (int k = 0; k < (j - i) / 2; k++)
+                    {
+                        char temp = phrase[k + i];
+                        phrase[k + i] = phrase[j - 1 - k];
+                        phrase[j - 1 - k] = temp;
+                    }
+
+                    i = j;
+                    break;
+                }
+            }
+        }
+    }
+
+    [Obsolete]
+    public static void ReversePhraseOld(ref char[] phrase)
+    {
         // if mutating the phrase, then find length of end word, save word to variable
         // shift right the length of the word and space and insert the word and a space.
 
@@ -50,6 +85,4 @@ public static class ReverseTest
         }
         phrase = newPhrase;
     }
-
-
 }
